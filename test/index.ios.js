7db3ee2ad78ee39c5652d396c9037f7c6225c859
jean <jean.ma.1986@gmail.com>
2016-10-28 18:00:06 +0800

    add TabView

--- test/index.ios.js
+++ test/index.ios.js
@@ -4,50 +4,88 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
+import TabView from "./tabView";
+
+class Tab1 extends Component {
+	componentDidMount() {
+		console.log("tab1 did mount")
+	}
+
+	componentWillUnmount() {
+		console.log("tab1 will unmount")
+	}
+
+	render() {
+		return <View style={{backgroundColor:"gray",flex:1}}><Text>1</Text></View>;
+	}
+}
+
+class Tab2 extends Component {
+	componentDidMount() {
+		console.log("tab2 did mount")
+	}
+
+	componentWillUnmount() {
+		console.log("tab2 will unmount")
+	}
+
+	render() {
+		return <View style={{backgroundColor:"lightgray",flex:1}}><Text>2</Text></View>;
+	}
+}
 
 export default class test extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+				<View style={{height:60,backgroundColor:"green"}}></View>
+				<TabView
+					style={{height:300}}
+					tabs={[{
+						text:"tab1",
+						onTabBarPress:()=>{},
+						component:<Tab1/>
+					  },{
+						text:"tab2",
+						component:<Tab2/>
+					  }]}
+								renderTabBar={(isActive,tab)=>{
+						console.log("render tab bar")
+						if(isActive){
+						  return <Text style={{textAlign:"center"}}>{tab.text}</Text>
+						}
+						return <Text style={{backgroundColor:"yellow",textAlign:"center"}}>{tab.text}</Text>
+					  }}>
+				</TabView>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 AppRegistry.registerComponent('test', () => test);
